0510cbfd74d2b776241d870b54d844639fb0a771
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-06-01 20:22:52 +0200

    fixes for better display

--- sncf.js
+++ sncf.js
@@ -1,7 +1,10 @@
 const apiKey = 'de22144e-af1e-4087-8e58-82e24de2abc0'
+const stationStopArea = 'stop_area:SNCF:87686006'
+const hiddenUris = ['network:SNCF:TNRER', 'network:SNCF:TN']
+const servicesCount = 12
 
 const board = new Board(document.querySelector('#board'), {
-  count: 12,
+  count: servicesCount,
   size: 25,
   delay: 50,
 })
@@ -24,8 +27,24 @@ const getMessageFromDeparture = ({ display_informations, stop_date_time }) => {
   return info
 }
 
+async function getStationData() {
+  const url = `https://api.sncf.com/v1/coverage/sncf/stop_areas/${stationStopArea}/`
+  const { stop_areas: stopAreas } = await makeApiCall(url)
+
+  return stopAreas[0].name
+}
+
 async function updateTrains() {
-  const url = `https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:OCE:SA:87271007/departures`
+  const forbiddenUrisQueryString = hiddenUris.map((x) => `&forbidden_uris[]=${x}`).join('')
+  const queryString = `count=${servicesCount}${forbiddenUrisQueryString}`
+  const url = `https://api.sncf.com/v1/coverage/sncf/stop_areas/${stationStopArea}/departures?${queryString}`
+  const { departures } = await makeApiCall(url)
+
+  const messages = departures.map(getMessageFromDeparture)
+  board.updateMessages(messages)
+}
+
+async function makeApiCall(url) {
   const response = await fetch(url, {
     method: 'get',
     headers: {
@@ -33,10 +52,17 @@ async function updateTrains() {
     },
   })
   const data = await response.json()
-  const messages = data.departures.map(getMessageFromDeparture)
-  board.updateMessages(messages)
+  return data
 }
 
 updateTrains()
+
+const stationNameElement = document.querySelectorAll('[data-station-name]')
+getStationData().then((name) => {
+  stationNameElement.forEach((x) => {
+    x.innerText = name
+    x.setAttribute('data-station-name', name)
+  })
+})
 
 setInterval(updateTrains, 50000)
